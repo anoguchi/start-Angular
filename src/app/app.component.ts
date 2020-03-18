163d464828247751cdd11ff89288d4a671487c30
Beto <alberto2611@gmail.com>
2020-03-18 00:27:58 -0300

    chore: criando o primeiro componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'noguchi-pic';
+  title = "Fadas";
+
+  photos = [
+    {
+      url: 'https://pbs.twimg.com/media/EIKRkzNX0AAnaWe.jpg',
+      title: 'Pokimane'
+    },
+    {
+      url: 'https://i.pinimg.com/originals/e8/78/94/e87894bacd7394dbd1a38ca53c5ef7bc.jpg',
+      title: 'WonderWoman'
+    }
+  ]
+
 }
